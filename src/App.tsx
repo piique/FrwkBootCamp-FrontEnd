f7555b80c7c928ffa4993f25974052d96479af7d
Victor <victorafonso@frwk.com.br>
2021-10-01 12:01:50 -0300

    chore - Estrutura cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Cadastro } from './features/cadastro/Cadastro'
 
 function App() {
-  return <div className="App">Hello World Frameworker!!</div>
+  return <div className="App"><Cadastro/></div>
 }
 
 export default App
